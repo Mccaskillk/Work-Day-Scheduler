b10399c7b21f18e7330165badbed977446c8c9fb
Kenneth McCaskill <kennethmccaskill@Kenneths-MBP.attlocal.net>
2022-09-02 00:39:19 -0500

    App is now working

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,63 @@
 
+
 //Current Month, Date & Year
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+
+//Loops over the time blocks
+var momentTime = moment().startOf("day").add(7, "hour");
+var hour = moment().format("H");
+
+for (var i = 8; i < 20; i++) {
+    var timeSlot = momentTime.add(1, "hour").format("HH:mm A");
+    var currentState;
+    
+  
+    // Color allows user to indicate whether it is in the past, present, or future on the specific timeblock.
+    if (hour == i) {
+        currentState = 'present';
+    } else if (hour > i) {
+        currentState = 'future';
+    } else if (hour < i) {
+        currentState = 'past';
+    }
+
+    var newStuff =
+        `<container class="row" id='hour-${i}'>
+            <div class="col-2"></div>
+            <div class="hour w-25 p-4 col-1">${timeSlot}</div>
+            <textarea class="description w-50 p-4 col-6 ${currentState} hour-${i}"></textarea>
+            <button class="saveBtn w-25 p-4 col-1 fas fa-save fa-2x"></button>
+            <div class="col-2">
+            </div>  
+        </container>
+        <p></p>`
+        ;
+
+    $("#containAll").append(newStuff);
+
+
+
+};
+// when the save button is clicked ,that event is saved into local storage 
+ $(".saveBtn").on("click", function(event) {
+    event.preventDefault();
+    saveID = $(this).data("save");
+    inputText = $(this).siblings().find("textarea").val();
+    localStorage.setItem(saveID, inputText);
+});
+
+// Loop to get item from from local storage 
+function showTask() {
+    for (var i = 9; i < 18; i++) {
+      var task = localStorage.getItem(i);
+      console.log(task);
+      $("#" + i + "").text(task);
+    }
+  }
+
+
+
+
 
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Work Day Scheduler Starter Code
+
+About
+This basic calendar allows for a user to save events for each hour of the day.
+The calendar is powered by HTML and CSSS powered by JQUERY.
+
+User Story
+- AS AN employee with a busy schedule
+- I WANT to add important events to a daily planner
+- SO THAT I can manage my time effectively
+
+
+Acceptance Criteria
+- GIVEN I am using a daily planner to create a schedule
+- WHEN I open the planner
+- THEN the current day is displayed at the top of the calendar
+- WHEN I scroll down
+- THEN I am presented with time blocks for standard business hours
+- WHEN I view the time blocks for that day
+- THEN each time block is color-coded to indicate whether it is in the past, present, or future
+- WHEN I click into a time block
+- THEN I can enter an event
+- WHEN I click the save button for that time block
+- THEN the text for that event is saved in local storage
+- WHEN I refresh the page
+- THEN the saved events persist
+
+
+<img width="1281" alt="Screen Shot 2022-09-02 at 12 22 48 AM" src="https://user-images.githubusercontent.com/110050573/188065503-0a62b266-5042-4599-9cf8-f9925eaf61ac.png">
+
+Contributors: Alex McCaskill
